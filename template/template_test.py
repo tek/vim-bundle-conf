285dad83fb56dbdd2b3bbff19e999c9a8a6da2f6
tek <tek@gentoo64.net>
2013-12-09 14:05:23 +0100

    remove python headers, move test template

---
+++ template/template_test.py
@@ -0,0 +1,16 @@
+import spec
+import sure
+from flexmock import flexmock
+
+from tek import Configurations, logger
+
+
+class <pythontestname>(spec.Spec, ):
+
+    def setup(self, *a, **kw):
+        pass
+
+    def test(self):
+        <+CURSOR+>pass
+
+__all__ = ['<pythontestname>']
