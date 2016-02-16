4c5830dbd4550589b61f72b3b168b69095da734c
tek <torstenschmits@gmail.com>
2016-02-16 19:57:55 +0100

    python template for _spec

---
+++ template/template_spec.py
@@ -0,0 +1,13 @@
+import sure  # NOQA
+from flexmock import flexmock  # NOQA
+
+
+class <pythontestname>(Spec):
+
+    def setup(self, *a, **kw):
+        super().setup(*a, **kw)
+
+    def test(self):
+        <+CURSOR+>pass
+
+__all__ = ('<pythontestname>')
